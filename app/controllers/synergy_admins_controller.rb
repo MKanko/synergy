ff14fcb38aa6087b5c3ededc0a2a3b22aa2d6894
“Mark <mkanko66@gmail.com>
2019-08-28 18:55:13 -0700

    add new, create, and strong params to synergy_admins_controller

--- app/controllers/synergy_admins_controller.rb
+++ app/controllers/synergy_admins_controller.rb
@@ -1,2 +1,22 @@
 class SynergyAdminsController < ApplicationController
+
+    def new
+        @synergy_admin = Synergy_admin.new 
+    end 
+
+    def create
+        @synergy_admin = Synergy_admin.new(synergy_admin_params)
+        return redirect_to new_synergy_admin_path unless @synergy_admin.save
+        redirect_to synergy_admin_path(@synergy_admin)
+    end 
+
+    def destroy
+    end
+    
+    private 
+
+    def synergy_admin_params
+        params.require(:synergy_admin).permit(:username, :email, :password)
+    end 
+
 end
