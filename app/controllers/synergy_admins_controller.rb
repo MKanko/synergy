667baaaa382b2285cb67e88b25f21fedd2eb2958
“Mark <mkanko66@gmail.com>
2019-08-28 19:38:13 -0700

    add show, destroy actions to synergy_admin_controller and add logged_in?, current_user helper methods to application_controller

--- app/controllers/synergy_admins_controller.rb
+++ app/controllers/synergy_admins_controller.rb
@@ -1,16 +1,27 @@
 class SynergyAdminsController < ApplicationController
 
-    def new
+    def new 
         @synergy_admin = Synergy_admin.new 
     end 
 
     def create
         @synergy_admin = Synergy_admin.new(synergy_admin_params)
         return redirect_to new_synergy_admin_path unless @synergy_admin.save
+        session[:synergy_admin_id] = @synergy_admin.id 
         redirect_to synergy_admin_path(@synergy_admin)
     end 
 
+    def show 
+        if !logged_in?
+            redirect_to new_synergy_admin_path 
+        else
+            @synergy_admin = Synergy_admin.find(params[:id])
+        end 
+    end 
+
     def destroy
+        session.delete :username
+        redirect_to new_synergy_admin_path 
     end
     
     private 
